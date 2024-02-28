89687c681cc4d38f46f177dd3848deeb52ec0ec3
gorans <gorans1993@gmail.com>
2024-02-28 15:27:14 +0100

    Added loging to requests

--- internal/api/v1/handler/wallet.go
+++ internal/api/v1/handler/wallet.go
@@ -30,11 +30,14 @@ func NewWalletHandler(walletService *service.WalletService) *WalletHandler {
 func (h *WalletHandler) GetBalance(c *gin.Context) {
 	walletID, _ := strconv.Atoi(c.Param("wallet_id"))
 
+	logrus.Infof("GetBalance request for wallet ID: %d", walletID)
+
 	cacheKey := fmt.Sprintf("wallet:%d:balance", walletID)
 	balanceStr, err := util.RedisClient.Get(c, cacheKey).Result()
 
 	if err == nil {
 		balance, _ := strconv.ParseFloat(balanceStr, 64)
+		logrus.Errorf("Error fetching balance for wallet ID %d: %s", walletID, err.Error())
 		c.JSON(200, gin.H{
 			"wallet_id": walletID,
 			"balance":   balance,
@@ -44,14 +47,16 @@ func (h *WalletHandler) GetBalance(c *gin.Context) {
 	}
 
 	balance, err := h.walletService.GetWalletBalance(walletID)
+	logrus.Infof("Balance fetched successfully for wallet ID %d", walletID)
 
 	if err != nil {
+		logrus.Infof("Error fetching balance from database for wallet ID %d", err)
 		c.JSON(500, gin.H{"error": err.Error()})
 		return
 	}
 
 	util.RedisClient.Set(c, cacheKey, fmt.Sprintf("%.2f", balance), time.Hour)
-
+	logrus.Infof("Balance fetched successfully for wallet ID %d", walletID)
 	c.JSON(200, gin.H{
 		"wallet_id": walletID,
 		"balance":   balance,
@@ -60,6 +65,7 @@ func (h *WalletHandler) GetBalance(c *gin.Context) {
 
 func (h *WalletHandler) Credit(c *gin.Context) {
 	walletID, _ := strconv.Atoi(c.Param("wallet_id"))
+	logrus.Infof("Credit request for wallet ID: %d", walletID)
 
 	// Clear cache when updating balance
 	cacheKey := fmt.Sprintf("wallet:%d:balance", walletID)
@@ -67,6 +73,7 @@ func (h *WalletHandler) Credit(c *gin.Context) {
 
 	amount, _ := strconv.ParseFloat(c.PostForm("amount"), 64)
 	if err := h.walletService.CreditWallet(walletID, amount); err != nil {
+		logrus.Errorf("Error crediting wallet ID %d: %s", walletID, err.Error())
 		c.JSON(500, gin.H{"error": err.Error()})
 		return
 	}
@@ -78,12 +85,14 @@ func (h *WalletHandler) Credit(c *gin.Context) {
 
 func (h *WalletHandler) Debit(c *gin.Context) {
 	walletID, _ := strconv.Atoi(c.Param("wallet_id"))
+	logrus.Infof("Debit request for wallet ID: %d", walletID)
 
 	cacheKey := fmt.Sprintf("wallet:%d:balance", walletID)
 	util.RedisClient.Del(c, cacheKey)
 
 	amount, _ := strconv.ParseFloat(c.PostForm("amount"), 64)
 	if err := h.walletService.DebitWallet(walletID, amount); err != nil {
+		logrus.Errorf("Error debiting wallet ID %d: %s", walletID, err.Error())
 		c.JSON(500, gin.H{"error": err.Error()})
 		return
 	}
